596a85b5a58e3b18645a451533cb321bed23db8c
Astronom43 <sergshubin@yandex.ru>
2020-03-15 01:33:22 +0300

    настроил слушателя очереди

--- src/test/java/ru/agronom/springboot_mail_service/service/MailRecServiceTest.java
+++ src/test/java/ru/agronom/springboot_mail_service/service/MailRecServiceTest.java
@@ -1,57 +1,74 @@
 package ru.agronom.springboot_mail_service.service;
 
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetup;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
 import ru.agronom.springboot_mail_service.domain.Message;
 
-import javax.mail.internet.MimeMessage;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@RunWith(MockitoJUnitRunner.class)
 class MailRecServiceTest {
 
-    @Autowired
-    MailRecService mailRecService;
 
     @Autowired
     RabbitTemplate template;
-
+    @Autowired
     Queue queue;
+    @Mock
+    MessageValidateService valid;
+    @Mock
+    MailSendService sendService;
+    @Autowired
+    MailRecService mailRecService;
 
-    private GreenMail greenMail;
-
-    Message message;
-
-
+    Message message, mrez;
 
 
     @BeforeEach
-    void init(){
-        greenMail = new GreenMail(new ServerSetup(2525, null, "smtp"));
-        greenMail.start();
-        greenMail.setUser("user", "pass");
-        queue = new Queue("mail",false);
-        message = new Message();
-        message.setFrom("s@s.com");
-        message.setTo("c@c.com");
-        message.setSubject("subject");
-        message.setText("text");
+    void initTest() {
 
+        message = new Message();
+        message.setFrom("s@s.s");
+        message.setTo("c@c.c");
+        message.setSubject("subj");
+        message.setText("text");
     }
 
     @Test
-    void receiveMessage() {
-        template.convertAndSend(queue.getName(),message);
-        MimeMessage[] rMessage = greenMail.getReceivedMessages();
-        assertEquals(1,rMessage.length);
+    void receiveMessage() throws InterruptedException {
+
+        Mockito.when(valid.isValid(any(Message.class))).thenReturn(true);
+        Mockito.doAnswer(invocationOnMock -> {
+            mrez = invocationOnMock.getArgument(0);
+            return null;
+        }).when(sendService).sendMessage(any(Message.class));
+
+        ReflectionTestUtils.setField(mailRecService,"validateService",valid);
+        ReflectionTestUtils.setField(mailRecService,"mailSendService",sendService);
+        template.convertAndSend(queue.getName(), message);
+        Thread.sleep(500);
+
+        Mockito.verify(valid,Mockito.times(1)).isValid(any(Message.class));
+
+       Mockito.verify(sendService, Mockito.times(1)).sendMessage(any(Message.class));
+
+       assertEquals(message.getText(), mrez.getText());
+
+
     }
 }
